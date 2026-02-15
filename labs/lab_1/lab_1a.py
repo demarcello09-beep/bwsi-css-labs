1caa6eabe51c22f8d070e3e0988bdbf6ea994280
Dominic-Marcello <demarcello09@gmail.com>
2026-02-15 14:37:58 -0800

    Added introduction to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Dominic Marcello" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hello, I'm Dominic, but I go by Dom, I'm a junior from California. I'm currently on the wrestling team, and in my free time I strive to try and accomplishing my goal of becoming an engineer. I look forward to learning with you guys.")
 
 if __name__ == "__main__":
     main()
